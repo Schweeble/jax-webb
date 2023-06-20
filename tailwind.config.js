8285d1328bb85e1f6ed23c6ca0c2b4e282e63957
schweeble <schweebledev@gmail.com>
2023-06-20 10:02:46 -0700

    rudimentary

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,27 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
     },
+    colors: {
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      gray: {
+        100: '#f7fafc',
+        // ...
+        900: '#1a202c',
+      },
+      red: '#dd0531'
+
+      // ...
+    },
+      
   },
   plugins: [],
 }
