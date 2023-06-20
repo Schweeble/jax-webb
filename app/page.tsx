0a801dfb74f9bf27d6d019dcc577645494cfeb16
schweeble <schweebledev@gmail.com>
2023-06-20 12:37:58 -0700

    Menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
+
 import Image from 'next/image'
 
-export default function Home() {
+
+const Home = () => {
   return (
-    <div className='justify-center'>
-      <h1>Welcome to JaxDesigns</h1>
-      <Image
-        src="/images/Jaxx Jewelry-12.jpg"
-        height={480}
-        width={480}
-        alt="Jax Jewelry"
-      />
-      
-    </div>
-    
+      <div className='justify-center'>
+        <h1>Welcome to JaxDesigns</h1>
+        <Image
+          src="/images/Jaxx Jewelry-12.jpg"
+          height={480}
+          width={480}
+          alt="Jax Jewelry"
+        />  
+        
+      </div>
   );
 }
+
+export default Home;
---
+++ components/menu.tsx
@@ -0,0 +1,148 @@
+'use client';
+
+import { AnimatePresence, Variants, motion } from 'framer-motion';
+import Link from 'next/link';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { GiSuperMushroom, GiChiliPepper, GiStrawberry } from 'react-icons/gi';
+
+import { MdOutlineMenuOpen, MdOutlineMenu } from 'react-icons/md';
+
+const MenuItemVariants: Variants = {
+  hide: {
+    opacity: 0,
+    x: 10,
+  },
+  show: {
+    opacity: 1,
+    x: 0,
+  },
+  exit: {
+    opacity: 0,
+    x: -10,
+  },
+};
+
+const iconBackgroundVariants: Variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
+
+type WithChildrenProps = {
+  children: React.ReactNode;
+};
+
+type IconLinkProps = WithChildrenProps;
+
+type AnimatedIconLinkProps = IconLinkProps & {
+  currentIndex: number | null;
+  index: number;
+  onEnter: (index: number | null) => void;
+};
+
+export const Menu = () => {
+  const [open, setOpen] = useState(true);
+
+  useEffect(() => {
+    console.log(open);
+  }, [open]);
+
+  return (
+    <motion.nav
+      className="flex flex-row justify-between gap-4 py-10 px-5 text-4xl text-white bg-red"
+      layout
+    >
+      <AnimatePresence mode="popLayout">
+        <motion.div
+          key="menu"
+          className="flex h-full w-full flex-row items-center justify-end bg-red"
+          variants={MenuItemVariants}
+          initial="show"
+          animate="show"
+          exit="exit"
+        >
+          {open ? (
+            <MenuOpen setOpen={setOpen} />
+          ) : (
+            <button
+              className="flex h-full justify-end bg-red text-xl"
+              onClick={() => setOpen(true)}
+            >
+              <MdOutlineMenu />
+            </button>
+          )}
+        </motion.div>
+      </AnimatePresence>
+    </motion.nav>
+  );
+};
+
+const MenuOpen = ({
+  setOpen,
+}: {
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const [social, setLink] = useState<number | null>(null);
+  const icons = [
+    [GiSuperMushroom, 'Home'],
+    [GiChiliPepper, 'Shop'],
+    [GiStrawberry, 'Contact'],
+  ];
+
+  return (
+    <div className='flex flex-row'>
+      <menu
+        className="flex flex-row items-center justify-center gap-4 text-3xl text-white"
+        onMouseLeave={() => setLink(null)}
+      >
+        {icons.map(([Icon, text], i) => (
+          <AnimatedIconLink
+            index={i}
+            key={i}
+            currentIndex={social}
+            onEnter={setLink}
+          >
+            <div className="flex space-x-2 items-center">
+              <Icon />
+              <Link href={'/' + (text === 'Home' ? "" : text.toString().toLowerCase())}>
+                {text.toString()}
+              </Link>
+            </div>
+          </AnimatedIconLink>
+        ))}
+      </menu>
+      <button className="p-4 text-xl items-center float-right relative" onClick={() => setOpen(false)}>
+        <MdOutlineMenuOpen />
+      </button>
+    </div>
+  );
+};
+
+export const AnimatedIconLink = ({
+  children,
+  currentIndex,
+  index,
+  onEnter,
+}: AnimatedIconLinkProps) => {
+  return (
+    <li
+      onMouseEnter={() => onEnter(index)}
+      key={index}
+      className="relative flex items-center justify-center overflow-visible rounded p-2 text-xl transition-all"
+    >
+      <AnimatePresence>
+        {index === currentIndex && (
+          <motion.div
+            className="absolute mx-auto h-full w-full rounded bg-pink-500"
+            layoutId="social"
+            key="social"
+            variants={iconBackgroundVariants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+          />
+        )}
+      </AnimatePresence>
+      <div className="relative z-10">{children}</div>
+    </li>
+  );
+};
